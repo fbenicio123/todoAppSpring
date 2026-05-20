f635386531154268e0cb5d1e7e235ca432686e1c
Felipe Abreu <idd_fbenicio@uolinc.com>
2026-05-20 13:22:14 -0300

    fix connection between apps

--- event-consumer/app.js
+++ event-consumer/app.js
@@ -1,41 +1,42 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const { Kafka } = require('kafkajs');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
+const PORT = 3000;
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// 1. Configuração do KafkaJS
+const kafka = new Kafka({
+  clientId: 'my-app',
+  brokers: ['localhost:19092'], // Endereço do seu broker Kafka
+  connectionTimeout: 10000,
+  requestTimeout: 30000,
+  retry: {
+    initialRetryTime: 3000,
+    retries: 10
+  }
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+const consumer = kafka.consumer({ groupId: 'kafka-broker-1' });
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// 2. Função para rodar o consumidor
+const runConsumer = async () => {
+  await consumer.connect();
+  await consumer.subscribe({ topic: 'user-events', fromBeginning: true });
+
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      console.log({
+        value: message.value.toString(),
+        partition,
+        topic
+      });
+      // Lógica de negócio aqui (ex: salvar no banco, atualizar cache)
+    },
+  });
+};
+
+// 3. Iniciar Express e Kafka
+app.listen(PORT, () => {
+  console.log(`Servidor Express rodando na porta ${PORT}`);
+  runConsumer().catch(console.error);
 });
-
-module.exports = app;
